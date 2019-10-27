2eb83576c6bd38016e3151dd81110fcaeef839b3
marpri-17 <mar.pnieto@gmail.com>
2019-10-27 12:57:51 +0100

    Fix github deplyment issue

---
+++ docs/precache-manifest.61267711f12f2aa6a356c14fa1e3c5eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74b55e1a34f1299def26d0750d793ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7200932e4f67655f213",
+    "url": "./static/css/main.6926b9c3.chunk.css"
+  },
+  {
+    "revision": "22e0bd6cb2b608267040",
+    "url": "./static/js/2.a3001a3e.chunk.js"
+  },
+  {
+    "revision": "b7200932e4f67655f213",
+    "url": "./static/js/main.4335b49f.chunk.js"
+  },
+  {
+    "revision": "62c5b51341087ef56d17",
+    "url": "./static/js/runtime-main.b71c1fa4.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d95d50186ef05f7fede65e948f1f5d09.js"
+  "./precache-manifest.61267711f12f2aa6a356c14fa1e3c5eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
